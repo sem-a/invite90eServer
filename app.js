e2f165971b584362b2a55a3429f7bcc77955ddd8
Алексей С. <76011034+sem-a@users.noreply.github.com>
2025-01-07 19:30:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.use(cors({
 
 app.use(logger("dev"));
 
+app.use(express.static('public'));
+
+// Отправляем файл index.html при запросе к /
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
