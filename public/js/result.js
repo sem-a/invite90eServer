35e2b7357c73d7e07f21abc4441717482381e17a
Алексей С. <76011034+sem-a@users.noreply.github.com>
2025-01-07 20:00:53 +0300

    Update result.js

--- public/js/result.js
+++ public/js/result.js
@@ -1,7 +1,7 @@
 // Функция для получения списка гостей
 function fetchGuestList() {
   // Выполняем GET-запрос на /api/get
-  fetch('http://sem-a-invite90eserver-f53e.twc1.net/api/get')
+  fetch('https://sem-a-invite90eserver-f53e.twc1.net/api/get')
       .then(response => {
           if (!response.ok) {
               throw new Error('Сеть ответила с ошибкой: ' + response.status);
