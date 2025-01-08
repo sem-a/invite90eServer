528675d7fc1fb403a05a5b06453051377aa392ce
Алексей С. <76011034+sem-a@users.noreply.github.com>
2025-01-08 12:30:21 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,16 +7,13 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://sem-a.github.io/inivite90e/', // Укажите разрешённый источник
+  methods: ['GET', 'POST'], // Разрешите необходимые методы
+  allowedHeaders: ['Content-Type'] // Разрешите заголовки по необходимости
+}));
 
 app.use(logger("dev"));
-
-app.use(express.static('public'));
-
-// Отправляем файл index.html при запросе к /
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- public/js/result.js
+++ public/js/result.js
@@ -1,33 +1,36 @@
 // Функция для получения списка гостей
 function fetchGuestList() {
-    // Выполняем GET-запрос на /api/get
-    fetch('/api/get')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Сеть ответила с ошибкой: ' + response.status);
-            }
-            return response.json();  // Преобразуем ответ в JSON
-        })
-        .then(data => {
-            const guestListContainer = document.querySelector('.guest-list');
-            
-            // Очищаем контейнер перед добавлением новых элементов (если нужно)
-            guestListContainer.innerHTML = '';
+  // Выполняем GET-запрос на /api/get
+  fetch("http://sem-a-invite90eserver-f53e.twc1.net/api/get")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Сеть ответила с ошибкой: " + response.status);
+      }
+      return response.json(); // Преобразуем ответ в JSON
+    })
+    .then((data) => {
+      const guestListContainer = document.querySelector(".guest-list");
 
-            data.forEach(guest => {  // Предполагается, что data - это массив объектов гостей
-                const listItem = document.createElement('div');  // Создаем новый элемент div для каждого гостя
-                
-                listItem.textContent = `${guest.name} (${guest.count})`;  // Устанавливаем текст элемента как имя и количество
+      // Очищаем контейнер перед добавлением новых элементов (если нужно)
+      guestListContainer.innerHTML = "";
 
-                guestListContainer.appendChild(listItem);  // Добавляем элемент в список гостей
-            });
-        })
-        .catch(error => {
-            console.error('Ошибка при получении списка гостей:', error);
-        });
+      data.forEach((guest) => {
+        // Предполагается, что data - это массив объектов гостей
+        const listItem = document.createElement("div"); // Создаем новый элемент div для каждого гостя
+
+        console.log(guest);
+
+        listItem.textContent = guest.name; // Устанавливаем текст элемента как имя гостя
+
+        guestListContainer.appendChild(listItem); // Добавляем элемент в список гостей
+      });
+    })
+    .catch((error) => {
+      console.error("Ошибка при получении списка гостей:", error);
+    });
 }
 
 // Вызываем функцию после загрузки страницы или по вашему усмотрению.
-document.addEventListener('DOMContentLoaded', () => {
-    fetchGuestList();
+document.addEventListener("DOMContentLoaded", () => {
+  fetchGuestList();
 });
